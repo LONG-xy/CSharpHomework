8eac13dadef77a146d554938e812904ec9d4a892
xiaoyi long <1260269038@qq.com>
2020-03-09 17:03:28 +0800

    *

---
+++ Homework2/Array_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Array_2
+{
+    class Program
+    {
+        static int Max(int[] array )
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+                else
+                    continue;
+            }
+            return max;
+        }
+        static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+                else
+                    continue;
+            }
+            return min;
+        }
+        static int Sum(int[] array)
+        {
+            int sum = 0;
+            foreach (int element in array)
+                sum += element;
+            return sum;
+        }
+        
+        
+        static void Main( )
+        {
+            int[] array = {1,92,123,2,234,89,3,345,567 };
+            Console.WriteLine("数组："+string.Join("  ", array));
+            int a = Max(array);
+            Console.WriteLine("该数组的最大值为{0}", a);
+            int b = Min(array);
+            Console.WriteLine("该数组的最小值为{0}", b);
+            int c = Sum(array);
+            Console.WriteLine("该数组的数值和为{0}", c);
+            Console.WriteLine($"该数组的平均值为{c/(array.Length)}" );
+
+
+        }
+    }
+}
---
+++ Homework2/PrimeNumber_1/assignment_1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumber
+{
+
+    //一个数若可以进行因数分解，那么分解时得到的两个数一定是一个小于等于sqrt(num)，一个大于等于sqrt(n)，
+    //据此，遍历到sqrt(num)即可，因为若sqrt(num)左侧找不到约数，那么右侧也一定找不到约数。
+
+    class Program
+    {   static bool IsPrime(int i)//判断一个数是否为质数
+        {
+            bool flag = true;
+            for(int n=2;n<i;n++)
+            {
+                if (i % n == 0)
+                { flag = false; break; }
+                else
+                    flag = true;
+            }
+            return flag;
+
+        }
+
+        static void GetPrimeFactor(int num)//求这个数的质数因子
+        {
+            int j = (int)Math.Sqrt(num);
+        
+                for (int i = 2; i < j; i++)//求这个数的因子
+                {
+                   
+                    if (num % i == 0)
+                    {
+                        if (IsPrime(i) == true)//该因子是否为质数
+                            Console.Write(i + " ");
+                        else
+                            continue;
+                    }
+                   
+                }
+            
+        }
+        static void Main(string[] args)
+            {
+                Console.WriteLine("请输入一个数");
+
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("这个数的质因数是");
+                GetPrimeFactor(num);
+            }
+        
+    }
+}
